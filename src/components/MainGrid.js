da80ec880cf3953680cdfe9606369641dc2c677e
Michael <stall84@gmail.com>
2020-10-18 02:11:52 -0400

    Working to resolve render of day-rows and hour columns on new Dev branch

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -10,37 +10,46 @@ const log = console.log;
 
 
 const Boxes = () => {
-    const [ columns ] = useState(6);
-    const [ rows ] = useState(6);
-
+    // const [ rows ] = useState(6);
+    // const [ columns ] = useState(4);
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const twentyFourHrArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];
     let row = [];
-    let cell = [];
-    for (var i = 0; i < columns; i++) {
+    let cells = [];
 
-        let rowID = `row${i}`
-        for (var k = 0; k < rows; k++) {
-            let cellID = `cell${i}-${k}`
-            cell.push(<td key={cellID} id={cellID} 
-                 style={styles.cellStyle}
-                 onClick={e => log( `You Clicked Cell: ${e.target.id}`) }>
-                     /
-                 </td>)
-                 
-        }
-        row.push(<tr key={i} id={rowID}>{cell}</tr>)
+    for (var i = 0; i < days.length; i++) {
+        
+            
+    cells.push(<div style={styles.cellStyle}>{twentyFourHrArr.toString()}</div>);
+        
     }
-        log('Rows Array: ', rows);
-        log('Cells Array: ', cell);
+
+    // for (var i = 0; i < columns; i++) {
+
+    //     let rowID = `row${i}`
+    //     for (var k = 0; k < rows; k++) {
+    //         let cellID = `cell${i}-${k}`
+    //         cell.push(<td key={cellID} id={cellID} 
+    //              style={styles.cellStyle}
+    //              onClick={e => log( `You Clicked Cell: ${e.target.id}`) }>
+    //                  /
+    //              </td>)
+                 
+    //     }
+    //     row.push(<tr key={i} id={rowID}>{cell}</tr>)
+    // }
+        // log('Rows Array: ', row);
+        // log('Cells Array: ', cell);
 
     return (
         
             <>
                     <div className="board" style={styles.board}></div>
-                        <table id="simple-board">
-                            <tbody>
-                                {row}
-                            </tbody>
-                        </table>    
+                        <div id="simple-board">
+                            <div>
+                                {cells}
+                            </div>
+                        </div>    
             </>
         
         )
